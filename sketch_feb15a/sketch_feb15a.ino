0cba03a88988157edaba2e6382f7def2ddb5e42b
wwwacedo <eng.marianammacedo@gmail.com>
2025-02-15 22:41:30 -0300

    json e map

--- sketch_feb15a/sketch_feb15a.ino
+++ sketch_feb15a/sketch_feb15a.ino
@@ -1,33 +1,53 @@
 #include <DHT11.h>
+#include <ArduinoJson.h>
+
+// Sensor DHT11 conectado ao pino digital 2
 DHT11 dht11(2);
 
+// Sensor de luminosidade no pino analógico A0
+const int analogInPin = A0;
+
+// Objeto JSON para armazenar os dados dos sensores
+JsonDocument sensor;
+
 void setup() {
-  // Initialize serial communication to allow debugging and data readout.
-  // Using a baud rate of 9600 bps.
+  // Inicia a comunicação serial com taxa de 9600 bps
   Serial.begin(9600);
 
-  // Uncomment the line below to set a custom delay between sensor readings (in milliseconds).
-  dht11.setDelay(4000);  // Set this to the desired delay. Default is 500ms.
+  // Define um intervalo de 4000ms entre leituras do DHT11 (o padrão é 500ms)
+  dht11.setDelay(4000);
 }
 
 void loop() {
-  int temperature = 0;
-  int humidity = 0;
+  int temperatura = 0;
+  int umidade = 0;
 
-  // Attempt to read the temperature and humidity values from the DHT11 sensor.
-  int result = dht11.readTemperatureHumidity(temperature, humidity);
+  // Realiza a leitura da temperatura e umidade do DHT11 a cada 4000ms
+  int result = dht11.readTemperatureHumidity(temperatura, umidade);
 
-  // Check the results of the readings.
-  // If the reading is successful, print the temperature and humidity values.
-  // If there are errors, print the appropriate error messages.
+  // Lê o valor do sensor de luminosidade em ohms
+  int luminosidade = map(analogRead(analogInPin), 0, 1023, 0, 10000);
+
+  // Verifica se a leitura foi bem-sucedida
   if (result == 0) {
-    Serial.print("Temperatura: ");
-    Serial.print(temperature);
-    Serial.print(" °C\tUmidade: ");
-    Serial.print(humidity);
-    Serial.println(" %");
+    sensor["temperatura"] = temperatura;
+    sensor["umidade"] = umidade;
+    sensor["luminosidade"] = luminosidade;
+
+    // Generate the minified JSON and send it to the Serial port.
+    serializeJson(sensor, Serial);
+
+     // Start a new line
+    Serial.println();
+
   } else {
-    // Print error message based on the error code.
+    // Exibe mensagem de erro caso a leitura do DHT11 falhe
+    Serial.print("Erro na leitura do DHT11: ");
     Serial.println(DHT11::getErrorString(result));
   }
 }
+
+
+
+
+
